24b9dd1caa2cb5630fb86a7d78eca2bd540803c5
dtd1614 <dtd1614@gmail.com>
2024-03-16 20:25:09 +0900

    0316

---
+++ Design_Pattern/src/factory_method/Main.java
@@ -0,0 +1,17 @@
+package factory_method;
+import factory_method.framework.Factory;
+import factory_method.framework.Product;
+import factory_method.idcard.IDCardFactory;
+import factory_method.idcard.IDCard;
+
+public class Main {
+	public static void main(String[] args) {
+		Factory factory = new IDCardFactory();
+		Product card1 = factory.create("Youngjin Kim");
+		Product card2 = factory.create("Heungmin Son");
+		Product card3 = factory.create("Kane");
+		card1.use();
+		card2.use();
+		card3.use();		
+	}
+}
